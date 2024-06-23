79211e59a5ace6890fa30224bf0b6909430461d2
R19G <guseynov_rustam2017@mail.ru>
2024-06-23 23:23:59 +0300

    Add files via upload

---
+++ main.ru1.py
@@ -0,0 +1,27 @@
+#задача "Длина слова"
+a="I am from Tver"
+print(len(a))
+
+#задача "Суммы и разности"
+first=5
+second=17
+summa=(first + second)
+diff=(first - second)
+print(summa, diff)
+
+#задача "Среднее арифметическое"
+third=22
+mean=((first + second + third) / 3)
+print(mean)
+
+#задача "Простые строчки"
+first_string="Вторник"
+second_string="Понедельник"
+print(second_string,",", first_string)
+
+#задача "Сложная функция"
+b=4
+c=7
+d=11
+f=((b * c) + (b * d))
+print(f ** 3)
